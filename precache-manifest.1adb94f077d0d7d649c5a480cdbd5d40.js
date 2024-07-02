293301fc9ddc4d131c4453413312ee597c77b12b
Ryan Laidlaw <rdl1028@usnh.edu>
2024-07-02 19:22:43 -0400

    Updates

---
+++ precache-manifest.1adb94f077d0d7d649c5a480cdbd5d40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9274e1a8708e343c230f4dcf4edaf3ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45df486fe2a79b031671",
+    "url": "/static/css/main.c89deca6.chunk.css"
+  },
+  {
+    "revision": "5bcfd37959659fdef79d",
+    "url": "/static/js/2.dc340470.chunk.js"
+  },
+  {
+    "revision": "5b5980e65c43315627b307fb4e094bc0",
+    "url": "/static/js/2.dc340470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c30d39a069ae18270e",
+    "url": "/static/js/3.8c6c03c6.chunk.js"
+  },
+  {
+    "revision": "45df486fe2a79b031671",
+    "url": "/static/js/main.5af0c882.chunk.js"
+  },
+  {
+    "revision": "590a54a07805d57b62cc",
+    "url": "/static/js/runtime-main.dbbe5933.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PersonalSite/precache-manifest.42532eed5774c9f49285efad6cef8877.js"
+  "/precache-manifest.1adb94f077d0d7d649c5a480cdbd5d40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PersonalSite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
