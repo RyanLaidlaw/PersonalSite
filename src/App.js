e6063bb380d1899c0dd10f17f2a341ccd76cf3a2
Ryan Laidlaw <rdl1028@usnh.edu>
2024-07-02 17:45:35 -0400

    title for link

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <div className="App">
       <Helmet>
+          <title>Ryan Laidlaw</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Helmet>
         <Navbar />
